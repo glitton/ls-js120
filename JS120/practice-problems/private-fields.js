18704bdcd621993448bc62fbdf344e1abce254ef
Generosa Litton <glcodeworks@gmail.com>
2025-07-06 15:51:46 -0700

    static methods

--- JS120/practice-problems/private-fields.js
+++ JS120/practice-problems/private-fields.js
@@ -88,5 +88,113 @@ class Book {
 // }
 
 class BankAccount {
-  
+  #balance = 0;
+
+  #checkBalance() {
+    console.log(`Current balance: ${this.#balance}`);
+  }
+
+  deposit(amount) {
+    this.#balance += amount;
+    this.#checkBalance();
+  }
+
+  withdraw(amount) {
+    if (amount > this.#balance) {
+      throw new RangeError("Insufficient funds");
+    } else {
+      this.#balance -= amount;
+      this.#checkBalance();
+    }
+  }
 }
+
+// let account = new BankAccount();
+// account.deposit(100); // Current balance: $100
+// account.withdraw(50); // Current balance: $50
+// account.withdraw(100); // Insufficient funds
+
+class Rectangle {
+  #width;
+  #height;
+
+  constructor(width, height) {
+    // calls the setter
+    this.width = width;
+    this.height = height;
+  }
+
+  get width() {
+    return this.#width;
+  }
+
+  set width(newWidth) {
+    if (newWidth <= 0) {
+      throw new RangeError("Width must be positive");
+    } else {
+      this.#width = newWidth;
+    }
+  }
+
+  get height() {
+    return this.#height;
+  }
+
+  set height(newHeight) {
+    if (newHeight <= 0) {
+      throw new RangeError("Height must be positive");
+    } else {
+      this.#height = newHeight;
+    }
+  }
+
+  get area() {
+    return this.width * this.height;
+  }
+}
+
+// let rect = new Rectangle(10, 5);
+// console.log(rect.area); // 50
+
+// rect.width = 20;
+// console.log(rect.area); // 100
+
+// rect.height = 12;
+// console.log(rect.area); // 240
+
+// try {
+//   rect.width = 0;
+// } catch (e) {
+//   console.log(e); // RangeError: width must be positive
+// }
+
+// try {
+//   rect.height = -10;
+// } catch (e) {
+//   console.log(e); // RangeError: height must be positive
+// }
+
+class MathUtils {
+  static add(num1, num2) {
+    return num1 + num2;
+  }
+  static subtract(num1, num2) {
+    return num1 - num2;
+  }
+  static multiply(num1, num2) {
+    return num1 * num2;
+  }
+  static divide(num1, num2) {
+    if (num2 === 0) {
+      throw new RangeError("Can't divide by zero");
+    } else {
+      return num1 / num2;
+    }
+  }
+}
+
+console.log(MathUtils.add(5, 3));
+console.log(MathUtils.subtract(10, 4)); // 6
+console.log(MathUtils.multiply(6, 7)); // 42
+console.log(MathUtils.divide(20, 5)); // 4
+console.log(MathUtils.divide(10, 0)); // RangeError: Division by zero
