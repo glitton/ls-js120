d7d58e1ed3f39b6c0c60154818aac133bba0dc0c
Generosa Litton <glcodeworks@gmail.com>
2025-08-04 18:27:01 -0500

    class syntax practice

---
+++ JS120/practice-problems/sub-inheritance-prac.js
@@ -0,0 +1,25 @@
+function Greeting() {}
+
+Greeting.prototype.greet = function (message) {
+  console.log(message);
+};
+
+function Hello() {}
+
+Hello.prototype = Object.create(Greeting.prototype);
+
+Hello.prototype.hi = function () {
+  this.greet("Hello!");
+};
+
+function Goodbye() {}
+
+Goodbye.prototype = Object.create(Greeting.prototype);
+
+Goodbye.prototype.bye = function () {
+  this.greet("Goodbye");
+};
+
+//Case 1:
+let hello = new Hello();
+hello.hi(); //no constructor
