d99a9b5748f81e181a1deac3551a2f122c71ec6f
Generosa Litton <glcodeworks@gmail.com>
2025-08-05 17:51:40 -0500

    rps game using protoype and constructors

---
+++ JS120/rps_constructors_classes/rps-proto-const.js
@@ -0,0 +1,60 @@
+function RPSGame() {
+  this.human = createHuman();
+  this.computer = createComputer();
+}
+
+RPSGame.prototype = {
+  displayWelcomeMessage() {
+    console.log("Welcome to Rock, Paper, Scissors!");
+  },
+  displayGoodbyeMessage() {
+    console.log("Thanks for playing Rock, Paper, Scissors. Goodbye!");
+  },
+
+  displayWinner() {
+    console.log(`You chose: ${this.human.move}`);
+    console.log(`The computer chose: ${this.computer.move}`);
+
+    let humanMove = this.human.move;
+    let computerMove = this.computer.move;
+
+    if (
+      (humanMove === "rock" && computerMove === "scissors") ||
+      (humanMove === "paper" && computerMove === "rock") ||
+      (humanMove === "scissors" && computerMove === "paper")
+    ) {
+      console.log("You win!");
+    } else if (
+      (humanMove === "rock" && computerMove === "paper") ||
+      (humanMove === "paper" && computerMove === "scissors") ||
+      (humanMove === "scissors" && computerMove === "rock")
+    ) {
+      console.log("Computer wins!");
+    } else {
+      console.log("It's a tie");
+    }
+  },
+
+  playAgain() {
+    console.log("Would you like to play again? (y/n)");
+    let answer = readline.question();
+    return answer.toLowerCase()[0] === "y";
+  },
+
+  play() {
+    this.displayWelcomeMessage();
+    while (true) {
+      this.human.choose();
+      this.computer.choose();
+      this.displayWinner();
+      if (!this.playAgain()) break;
+    }
+
+    this.displayGoodbyeMessage();
+  },
+};
+
+RPSgame.prototype.constructor = RPSGame
+
+let game = new RPSGame()
+game.play()
