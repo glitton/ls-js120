9c227cbb2beeb9d93d97cf894c4de8069f0b84d0
Generosa Litton <glcodeworks@gmail.com>
2025-08-10 14:22:21 -0500

    board and markers creating

--- JS120/oo_tictactoe/tictactoe-class.js
+++ JS120/oo_tictactoe/tictactoe-class.js
@@ -1,47 +1,71 @@
-class Board {
-  constructor() {
-    //STUB
-    // We need a way to model the 3x3 grid. Perhaps "squares"?
-    // What data structure should we use? An Array? An Object? Something else?
-    // What should the data structure store? Strings? Numbers? Square objects?
+class Square {
+  static UNUSED_SQUARE = " ";
+  static HUMAN_MARKER = "X";
+  static COMPUTER_MARKER = "O";
+
+  constructor(marker = Square.UNUSED_SQUARE) {
+    this.marker = marker;
+  }
+
+  toString() {
+    return this.marker;
   }
 }
 
-class Square {
+class Board {
   constructor() {
-    //STUB
-    // We need some way to keep track of this square's marker.
+    this.squares = {};
+    for (let counter = 1; counter <= 9; counter++) {
+      this.squares[String(counter)] = new Square();
+    }
+  }
+
+  display() {
+    console.log("");
+    console.log("     |     |");
+    console.log(
+      `  ${this.squares["1"]}  |  ${this.squares["2"]}  |  ${this.squares["3"]}`
+    );
+    console.log("     |     |");
+    console.log("-----+-----+-----");
+    console.log("     |     |");
+    console.log(
+      `  ${this.squares["4"]}  |  ${this.squares["5"]}  |  ${this.squares["6"]}`
+    );
+    console.log("     |     |");
+    console.log("-----+-----+-----");
+    console.log("     |     |");
+    console.log(
+      `  ${this.squares["7"]}  |  ${this.squares["8"]}  |  ${this.squares["9"]}`
+    );
+    console.log("     |     |");
+    console.log("");
   }
 }
 
 class Row {
   constructor() {
-    //STUB
     // We need some way to identify a row of 3 squares
   }
 }
 
 class Marker {
   constructor() {
-    //STUB
     // A marker is something that represents a player's "piece" on the board.
   }
 }
 
 class Player {
   constructor() {
-    //STUB
     // maybe a "marker" to keep track of this player's symbol (i.e., 'X' or 'O')
   }
 
   mark() {
-    //STUB
     // We need a way to mark the board with this player's marker.
     // How do we access the board?
   }
 
   play() {
-    //STUB
     // We need a way for each player to play the game.
     // Do we need access to the board?
   }
@@ -61,16 +85,14 @@ class Computer extends Player {
 
 class TTTGame {
   constructor() {
-    //STUB
-    // Need a board and two players
+    this.board = new Board();
   }
 
   play() {
-    //SPIKE
     this.displayWelcomeMessage();
 
     while (true) {
-      this.displayBoard();
+      this.board.display();
 
       this.firstPlayerMoves();
       if (this.gameOver()) break;
@@ -94,27 +116,18 @@ class TTTGame {
   }
 
   displayResults() {
-    //STUB
     // show the results of this game (win, lose, tie)
   }
 
-  displayBoard() {
-    //STUB
-    // display the board, including its current state
-  }
-
   firstPlayerMoves() {
-    //STUB
     // the first player makes a move
   }
 
   secondPlayerMoves() {
-    //STUB
     // the second player makes a move
   }
 
   gameOver() {
-    //STUB
     return false;
   }
 }
